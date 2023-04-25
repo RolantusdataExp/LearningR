80a2f3dbc98e6654a3eeab8a244067fbd4d9f7d1
Peter Roland Jakobsgaard <peter.roland@me.com>
2023-04-25 15:40:15 +0200

    HI here I change area

---
+++ R/learning.R
@@ -0,0 +1,14 @@
+# Basics of R -------------------------------------------------------------
+
+rownames(airquality)
+str(airquality)
+summary(airquality)
+
+1 + 1
+
+mean(2:6)
+
+
+# loading packages --------------------------------------------------------
+library(tidyverse)
+
