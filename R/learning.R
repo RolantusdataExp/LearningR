a6649dc8963be0f3f8d12eb20cfd9f99c574e7be
Peter Roland Jakobsgaard <peter.roland@me.com>
2023-04-26 13:21:11 +0200

    We did more!

--- R/learning.R
+++ R/learning.R
@@ -69,6 +69,42 @@ nhanes_small %>%
   select(starts_with("bp_")) %>%
   rename(bp_systolic = bp_sys_ave)
 
+# Filter
+
+nhanes_small %>%
+  filter(phys_active == "No")
+
+nhanes_small %>%
+  filter(bmi >= 25)
+
+# Combining logical operators
+
+nhanes_small %>%
+  filter(bmi >= 25 & phys_active == "No")
+
+nhanes_small %>%
+  filter(bmi >= 25 | phys_active == "No")
+
+# Arrange data
+
+nhanes_small %>%
+  arrange(age)
+
+nhanes_small %>%
+  arrange(desc(age))
+
+nhanes_small %>%
+  arrange(education, age)
+
+# Transforming data
+nhanes_small %>%
+  mutate(
+    age = age * 12,
+    logged_bmi = log(bmi)
+  )
+
+nhanes_small %>%
+  mutate(old = if_else(age > -30, "Yes", "No"))
 
 
 
